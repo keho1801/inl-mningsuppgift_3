331065c4bf0ef6ef6d79651ebf24c6c8dedf0691
Lovisa Lybeck <Lovisa.Lybeck@yh.nackademin.se>
2018-11-15 13:10:28 +0100

    Revert "- Added first classes: Player, Quest and GameController"
    This reverts commit c58f9a0e0e2f25b10cbbf66e4c624d8b1b2816ca.

--- src/QuizkampenKlient/Question.java
+++ src/QuizkampenKlient/Question.java
@@ -1,43 +1,13 @@
 package QuizkampenKlient;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class Question implements Serializable {
     private String question;
-    private String correctAnswer;
-    private String category;
-    private List<String> answers;
+    private String answer;
     
-    public Question(String question, String correctAnswer, List<String> answers, String category) {
+    public Question(String question, String answer) {
         this.question = question;
-        this.correctAnswer = correctAnswer;
-        this.category = category;
-        this.answers = answers;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-
-    public String getQuestion() {
-        return question;
-    }
-    
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getCorrectAnswer() {
-        return correctAnswer;
-    }
-    
-    public void setCorrectAnswer(String correctAnswer) {
-        this.correctAnswer = correctAnswer;
+        this.answer = answer;
     }
 }
