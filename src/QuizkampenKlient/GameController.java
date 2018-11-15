c58f9a0e0e2f25b10cbbf66e4c624d8b1b2816ca
Kristian Grundström <kristian.grundstrom@yh.nackademin.se>
2018-11-15 10:06:11 +0100

    - Added first classes: Player, Quest and GameController

--- src/QuizkampenKlient/GameController.java
+++ src/QuizkampenKlient/GameController.java
@@ -1,19 +1,33 @@
 package QuizkampenKlient;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GameController {
-    private List<Question> allQuestions = new ArrayList<>();
-    
-    public GameController() {
+    private List<Question> questionsDatabase = new ArrayList<>();
+    private List<Question> questionsInGame = new ArrayList<>();
+    int numberOfQuestions;
+            
+    public GameController(int nrOfQuestionsInGame) {
+        this.numberOfQuestions = nrOfQuestionsInGame;
         
-        allQuestions.add(new Question("Hur långt är ett snöre?", "3.1415"));
-        allQuestions.add(new Question("Svara ja.", "ja"));
-        allQuestions.add(new Question("Hur många frågor är det i detta spel?", "4"));
-        allQuestions.add(new Question("Stämde fråga 1?", "nej"));
-        allQuestions.add(new Question("Vad kommer efter 9?", "10"));
-        allQuestions.add(new Question("Ska det finnas ananas på pizza?", "nej"));
+//        questionsDatabase.add(new Question("Hur långt är ett snöre?", "3.1415"));
+//        questionsDatabase.add(new Question("Svara ja.", "ja"));
+//        questionsDatabase.add(new Question("Vem är statsminister?", "stefan lövèn"));
+//        questionsDatabase.add(new Question("Stämde fråga 1?", "nej"));
+//        questionsDatabase.add(new Question("Vad kommer efter 9?", "10"));
+//        questionsDatabase.add(new Question("Ska det finnas ananas på pizza?", "nej"));
+//        questionsDatabase.add(new Question("Fisk eller...?", "fågel"));
+//        questionsDatabase.add(new Question("Vad är motsatsen till över?", "under"));
+//        questionsDatabase.add(new Question("Vem ler på en känd tavla?", "mona-lisa"));
+        
+        Collections.shuffle(questionsDatabase);
+        
+        for (int i = 0; i < nrOfQuestionsInGame; i++) {
+            questionsInGame.add(questionsDatabase.get(i));
+            questionsDatabase.remove(i);
+        }
         
     }
 }
